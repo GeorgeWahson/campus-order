12b3e1e7f3cf1d015307602c55b7bcb3807c1898
GeorgeWahson <georgewahson@qq.com>
2024-01-14 16:47:34 +0800

    全局异常处理器

---
+++ campus-order-common/src/main/java/com/watson/order/exception/CustomException.java
@@ -0,0 +1,8 @@
+package com.watson.order.exception;
+
+public class CustomException extends RuntimeException{
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
---
+++ campus-order-common/src/main/java/com/watson/order/exception/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.watson.order.exception;
+
+import com.watson.order.dto.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理类
+ */
+@Slf4j
+@RestControllerAdvice
+// 上面一行 和下面两行 二选一
+//@ControllerAdvice(annotations = {RestController.class, Controller.class})
+//@RequestBody
+public class GlobalExceptionHandler {
+    /**
+     * 异常处理方法
+     * @return Result.error 错误信息
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public Result<String> exceptionHandler(SQLIntegrityConstraintViolationException e) {
+        log.error(e.getMessage());
+        // 如果报错信息中有“Duplicate entry”，说明用户名或分类名重复
+        if (e.getMessage().contains("Duplicate entry")) {
+            String[] split = e.getMessage().split(" ");
+            // Duplicate entry 'zhangsan'
+            String msg = "【" + split[2].substring(1, split[2].length()-1) + "】已存在，请换个名称!";
+            return Result.error(msg);
+        }
+        return Result.error("未知错误。。。");
+    }
+
+    /**
+     * 自定义异常处理方法
+     * @return
+     */
+    @ExceptionHandler(CustomException.class)
+    public Result<String> exceptionHandler(CustomException e) {
+        log.error(e.getMessage());
+
+        return Result.error(e.getMessage());
+    }
+}
