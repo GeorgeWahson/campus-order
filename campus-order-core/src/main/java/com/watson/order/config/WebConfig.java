d66e727cd6afeca2054d78e2c8ed40edc5c30553
GeorgeWahson <georgewahson@qq.com>
2024-02-04 16:07:20 +0800

    配置swagger-api文档注解

--- campus-order-core/src/main/java/com/watson/order/config/WebConfig.java
+++ campus-order-core/src/main/java/com/watson/order/config/WebConfig.java
@@ -1,13 +1,22 @@
 package com.watson.order.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.watson.order.interceptor.LoginCheckInterceptor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +24,8 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
@@ -31,7 +42,8 @@ public class WebConfig implements WebMvcConfigurer {
                 "/common/**",
                 "/user/sendMsg",
                 "/user/login",
-                "/doc.html",
+                "/favicon.ico",
+                "/doc.html",   // 放行 api 文档页面
                 "/webjars/**",
                 "/swagger-resources",
                 "/v2/api-docs"
@@ -59,4 +71,22 @@ public class WebConfig implements WebMvcConfigurer {
         converters.add(0, messageConverter);
     }
 
+    @Bean
+    public Docket createRestApi() {
+        // 文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.watson.order.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("校园点餐")
+                .version("1.0")
+                .description("校园点餐接口文档")
+                .build();
+    }
 }
