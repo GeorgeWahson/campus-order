be0f2fecd03dba92b00f19b7b01b0b2e14b4aed6
GeorgeWahson <georgewahson@qq.com>
2024-02-04 16:55:52 +0800

    format code

--- campus-order-core/src/main/java/com/watson/order/handler/MyMetaObjectHandler.java
+++ campus-order-core/src/main/java/com/watson/order/handler/MyMetaObjectHandler.java
@@ -17,16 +17,15 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("insertFill:  " + metaObject.toString());
-         this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
-         this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
-         this.strictInsertFill(metaObject, "createUser", Long.class, BaseContext.getCurrentId()); // 起始版本 3.3.0(推荐使用)
-         this.strictInsertFill(metaObject, "updateUser", Long.class, BaseContext.getCurrentId()); // 起始版本 3.3.0(推荐使用)
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
+        this.strictInsertFill(metaObject, "createUser", Long.class, BaseContext.getCurrentId()); // 起始版本 3.3.0(推荐使用)
+        this.strictInsertFill(metaObject, "updateUser", Long.class, BaseContext.getCurrentId()); // 起始版本 3.3.0(推荐使用)
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("updateFill:  " + metaObject.toString());
-        log.info("线程id为：{}", Thread.currentThread().getId());
         metaObject.setValue("updateTime", LocalDateTime.now());
         metaObject.setValue("updateUser", BaseContext.getCurrentId());
 
